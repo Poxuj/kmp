67f63503969623354e8332e6f0ff85742ddc9866
King <65786029+Poxuj@users.noreply.github.com>
2021-05-26 12:51:12 +0300

    Update events.cpp

--- proxy/events.cpp
+++ proxy/events.cpp
@@ -380,11 +380,8 @@ bool events::out::generictext(std::string packet) {
                 "\nadd_textbox|`b/pinfo (Proxy information) |left|2480|"
                 "\nadd_textbox|`b/autopull (auto pull when people enter world) |left|2480|"
                 "\nadd_spacer|small|"
-                " \nadd_url_button||`$KiNG Github``|NOFLAGS|https://github.com/Poxuj|Open link?|0|0|;
+                " \nadd_url_button||`$KiNG Github``|NOFLAGS|https://github.com/Poxuj|Open link?|0|0|"
                 "\nadd_spacer|small|
-	        "\nadd_url_button||`$FakeModz Github``|NOFLAGS|https:/github.com/FakeModz|Open link?|0|0|;
-                " \nadd_spacer|small|
-                " \nadd_url_button||`$Ama Github``|NOFLAGS|https:/github.com/ama6nen|Open link?|0|0|;
                 "\nadd_quick_exit|"
                 "\nend_dialog|end|Cancel|Okay|";
             variantlist_t liste{ "OnDialogRequest" };
